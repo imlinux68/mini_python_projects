5976f8bd066c22be63a89d76703e220648580122
im linux <imlinux68@gmail.com>
2023-05-14 22:26:03 +0300

    added cycle

--- Docker_Python1/main.py
+++ Docker_Python1/main.py
@@ -6,4 +6,5 @@ def sum(a, b):
 a = int(input('Enter 1st number: '))
 b = int(input('Enter 2nd number: '))
 
-print(f'Sum of {a} and {b} is {sum(a, b)}')
+for i in range(10):
+    print(f'{i + 1}\t Sum of {a} and {b} is {sum(a, b)}')
